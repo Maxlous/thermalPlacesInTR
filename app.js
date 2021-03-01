561cc5f6b23555238fd8fbb82c74b5f893c38e7d
Burak Gündüz <burakkgunduz21@gmail.com>
2021-03-01 18:18:34 +0300

    mongo sessions added, last touches for deployment have been made

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ const methodOverride = require("method-override");
 const thermalRoutes = require("./routes/thermal");
 const reviewRoutes = require("./routes/reviews");
 const session = require("express-session");
+const MongoStore = require('connect-mongo').default; 
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 const userRoutes = require("./routes/users");
 const mongoSanitize = require("express-mongo-sanitize");
-const helmet = require("helmet")
+const helmet = require("helmet");
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/thermalTr';
 
-mongoose.connect('mongodb://localhost:27017/thermalTr', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -31,12 +33,13 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once("open", () => {
     console.log("database connected!!!");
 });
-const app = express();
 
+const app = express();
+// ejs settings
 app.engine("ejs", ejsMate);
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views'));
-
+// route settings
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public" )));
@@ -44,24 +47,37 @@ app.use(mongoSanitize({
     replaceWith: '_'
   }));
 
+const secret = process.env.SECRET || "hereasecret";
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e) {
+    console.log("session store error")
+})
+
 const sessionConfig = {
+    store,
     name: "ourSession",
-    secret: "thisneedstobemodified",
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
         // secure: true,
-        expires: Date.now() + 1000 * 60 * 60* 24* 7,
+        expires: Date.now() + 1000 * 60 * 60* 24* 7,//in a week
         maxAge: 1000 * 60 * 60* 24* 7
     }
 }
+//more use
 app.use(session(sessionConfig))
 app.use(flash());
 app.use(helmet({
      frameguard: false
 }));
-
+// urls that can used in this app
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
     "https://api.tiles.mapbox.com/",
@@ -85,6 +101,7 @@ const connectSrcUrls = [
     "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
 ];
+//code for dealing with the contentSecurityPolicy
 const fontSrcUrls = [];
 app.use(
     helmet.contentSecurityPolicy({
@@ -99,20 +116,20 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/maxlous/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/maxlous/", 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
         },
     })
 );
-
+//related with auth
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
+//flashları tanımlıyoruz
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
@@ -137,7 +154,7 @@ app.use((err, req, res, next) => {
     if(!err.message) err.message = "Oh no something went wrong"
     res.status(statusCode).render("error", { err });
 } )
-
+//localhost 3000'de
 app.listen(3000, () => {
     console.log("Serving in port 3000")
 });
